b1aec55fe3445b131454a014db8fa8c1a8f9fad5
vannia <hvannia@gmail.com>
2019-03-21 20:39:20 -0700

    plots from geojson

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,6 +1,6 @@
 let streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
+    maxZoom: 6,
     id: "mapbox.streets",
     accessToken: API_KEY  
   });
@@ -14,10 +14,18 @@ let darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?
 
 let satellitemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",{
       attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-      maxZoom: 18,
+      maxZoom: 6,
       id: "mapbox.satellite",
       accessToken: API_KEY
   });
+
+
+let outdoorsmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+   maxZoom: 16,
+   id: "mapbox.outdoors",
+   accessToken: API_KEY
+});
 
 let mapStyle = {
     color: "white",
@@ -28,7 +36,7 @@ let mapStyle = {
 
 let pathMap = L.map("pathmap", {
     center: [31.7128, -41.0059],
-    zoom: 5,
+    zoom: 2,
     layers: [streetmap]
   });
 
